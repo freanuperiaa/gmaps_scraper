aa96adf750f2e6f848429941fa5d91218e68bba9
Freanu Peria <magnussonfrancis@gmail.com>
2020-01-14 13:56:57 +0800

    now able to go to other page of the result and scrape data.

--- core/scraper.py
+++ core/scraper.py
@@ -1,10 +1,14 @@
+import sys
 from time import sleep
 
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as exp_con
 import pandas as pd
 #
 # from exceptions import *
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def export_to_csv(data_frame):
@@ -13,38 +17,114 @@ def export_to_csv(data_frame):
     data_frame.to_csv(file_name)
 
 
-def scrape(driver):
+def scrape_from_page(driver):
     # check table where data is contained
     table_result = driver.find_elements(By.XPATH, '//table[@id="deals_filter_result"]/tbody/tr')
     # if table_result in [[], 0, None]:
     #     raise ElementNotFoundException
-    page_data = []  # variable to contain all rows
+    page_data = []  # variable to contain all rows in the page
+    curr_iteration = 0
     for row in table_result:
-        address = row.find_element_by_class_name('deals1').text
-        deals2 = row.find_element_by_class_name('deals2').text
-        tenant = row.find_element_by_class_name('deals3').text
-        landlord = row.find_element_by_class_name('deals4').text
-        borough = row.find_element_by_class_name('deals5').text
-        size = row.find_element_by_class_name('deals6').text
-        deals7 = row.find_element_by_class_name('deals7').text
-        deals8 = row.find_element_by_class_name('deals8').text
-        neighborhood = row.find_element_by_class_name('deals9').text
-        event_date = row.find_element_by_class_name('deals10').text
-        tenant_rep_agent = row.find_element_by_class_name('deals11').text
-        landlord_rep_agent = row.find_element_by_class_name('deals12').text
-        event = row.find_element_by_class_name('deals13').text
-        publication_date = row.find_element_by_class_name('deals14').text
-        tenant_rep_firm = row.find_element_by_class_name('deals15').text
-        landlord_rep_firm = row.find_element_by_class_name('deals16').text
+        curr_iteration += 1
+        try:
+            address = row.find_element_by_class_name('deals1').text
+        except Exception:
+            address = ''
+        try:
+            deals2 = row.find_element_by_class_name('deals2').text
+        except Exception:
+            deals2 = ''
+        try:
+            tenant = row.find_element_by_class_name('deals3').text
+        except Exception:
+            tenant = ''
+        try:
+            landlord = row.find_element_by_class_name('deals4').text
+        except Exception:
+            landlord = ''
+        try:
+            borough = row.find_element_by_class_name('deals5').text
+        except Exception:
+            borough = ''
+        try:
+            size = row.find_element_by_class_name('deals6').text
+        except Exception:
+            size = ''
+        try:
+            deals7 = row.find_element_by_class_name('deals7').text
+        except Exception:
+            deals7 = ''
+        try:
+            deals8 = row.find_element_by_class_name('deals8').text
+        except Exception:
+            deals8 = ''
+        try:
+            neighborhood = row.find_element_by_class_name('deals9').text
+        except Exception:
+            neighborhood = ''
+        try:
+            event_date = row.find_element_by_class_name('deals10').text
+        except Exception:
+            event_date = ''
+        try:
+            tenant_rep_agent = row.find_element_by_class_name('deals11').text
+        except Exception:
+            tenant_rep_agent = ''
+        try:
+            landlord_rep_agent = row.find_element_by_class_name('deals12').text
+        except Exception:
+            landlord_rep_agent = ''
+        try:
+            event = row.find_element_by_class_name('deals13').text
+        except Exception:
+            event = ''
+        try:
+            publication_date = row.find_element_by_class_name('deals14').text
+        except Exception:
+            publication_date = ''
+        try:
+            tenant_rep_firm = row.find_element_by_class_name('deals15').text
+        except Exception:
+            tenant_rep_firm = ''
+        try:
+            landlord_rep_firm = row.find_element_by_class_name('deals16').text
+        except Exception:
+            landlord_rep_firm = ''
         new_row = ((
             address, deals2, tenant, landlord, borough, size, deals7, deals8,
             neighborhood, event_date, tenant_rep_agent, landlord_rep_agent,
             event, publication_date, tenant_rep_firm, landlord_rep_firm
-       ))
-        print('processing_  ')
+        ))
+        sys.stdout.write("\r%d%%" % curr_iteration)
+        sys.stdout.flush()
         page_data.append(new_row)
-        # put the data in page_data to a data_frame
-    data_frame = pd.DataFrame(page_data, columns=[
+    return page_data
+
+
+def scrape(driver):
+    trd_data = []
+    # check if we're on the current page
+    curr_page = int(driver.find_element(By.XPATH, '//div[@class="deals_pagination_pages"]/span[@class="red"]').text)
+    # iterate through every page of the results
+    while curr_page <= 81:
+        print('current page:', curr_page)
+        print('no. of data scraped:', len(trd_data))
+        curr_page_data = scrape_from_page(driver)
+        for row_data in curr_page_data:
+            trd_data.append(row_data)
+        # next_page = driver.find_element(
+        #     By.XPATH, '//div[@class="deals_pagination_pages"]/span[@class="red"]//following::a[1]'
+        # )
+        next_page = WebDriverWait(driver, 15).until(
+            exp_con.presence_of_element_located((
+                By.XPATH, '//div[@class="deals_pagination_pages"]/span[@class="red"]//following::a[1]'
+            ))
+        )
+        next_page.click()
+        curr_page += 1
+        sleep(2)
+
+    data_frame = pd.DataFrame(trd_data, columns=[
         'Address', 'deals2', 'Tenant', 'Landlord', 'Borough', 'Size',
         'deals7', 'deals8', 'Neighborhood', 'Event Date', 'Tenant Rep. Agent',
         'Landlord Rep. Agent', 'Event', 'Publication Date', 'Tenant Rep. Firm',
@@ -70,7 +150,7 @@ def ready_scrape(driver):
     queens_checkbox.click()
     staten_island_checkbox.click()
     submit_button.click()
-    sleep(3)
+    sleep(2)
     scrape(driver)
 
 
