71449176f5cab507bf2d4a1fbf5d617d6d6d4bbe
Freanu Peria <magnussonfrancis@gmail.com>
2020-01-15 15:24:18 +0800

    parsing data from page now works. need to fix workflow. WIP.

--- core/google_scraper.py
+++ core/google_scraper.py
@@ -13,8 +13,9 @@ import pandas as pd
 
 
 class GoogleMapsCrawler:
-    NO_RESULTS = 5
+    NO_RESULTS = 20
     DRIVER_TITLE = 'nail salons in south queens new york'
+    data = []
 
     def get_waiter(self, driver, timeout=10):
         return ui.WebDriverWait(driver, timeout)
@@ -24,16 +25,72 @@ class GoogleMapsCrawler:
         file_name = input('enter file name for csv')
         data_frame.to_csv(file_name)
 
-    def scrape_results(self, driver):
+    def scrape_results(self, driver, curr_page):
+        wait = self.get_waiter(driver)  # init waiter for this method
+
         # i had to do this because google chrome seems to have a weird behaviour
         for x in range(self.NO_RESULTS):
             sys.stdout.write('{}th iteration\r'.format(x))
             sys.stdout.flush()
+            for x in range((curr_page-1)):
+                print('went to next page')
+                next_button = driver.find_element(By.XPATH, '//div/button[contains(@aria-label,"Next page")]')
+                if next_button.is_enabled():
+                    next_button.click()
+                try:
+                    sleep(5)
+                    wait.until(
+                        exp_con.presence_of_element_located((
+                            By.XPATH, '//div[@class="section-result"]'
+                        ))
+                    )
+                except Exception:
+                    try:
+                        sleep(5)
+                        wait.until(
+                            exp_con.presence_of_element_located((
+                                By.XPATH, '//div[@class="section-result"]'
+                            ))
+                        )
+                    except Exception:
+                        pass
             results = driver.find_elements(By.XPATH, '//div[@class="section-result"]')
             results[x].click()
-            sleep(3)
+            wait.until(
+                exp_con.presence_of_element_located((
+                    By.XPATH, '//h1[contains(@class, "title-title")]'
+                ))
+            )
+            try:
+                name = driver.find_element(By.XPATH, '//h1[contains(@class, "title-title")]').text
+            except Exception:
+                name = ''
+            try:
+                phone_number = driver.find_element(
+                    By.XPATH,
+                    '//span[@aria-label="Phone"]/following::span[@class="section-info-text"]'
+                    '[1]/span[contains(@class,"-link")]'
+                ).text
+            except Exception:
+                phone_number = ''
+            try:
+                address1 = driver.find_element(
+                    By.XPATH,
+                    '//span[@aria-label="Address"]/following::span[@class='
+                    '"section-info-text"][1]/span[contains(@class,"-link")]'
+                ).text
+            except Exception:
+                address1 = ''
+            try:
+                address2 = driver.find_element(
+                    By.XPATH,
+                    '//span[@aria-label="Address"]/following::span'
+                    '[@class="section-info-text"][2]/span[contains(@class,"-link")]'
+                ).text
+            except Exception:
+                address2 = ''
+            print(name, ' ', phone_number, ' ', address1, ' ', address2)
             driver.execute_script("window.history.go(-1)")
-            wait = self.get_waiter(driver)
             # start of nested try-catch statements
             try:
                 wait.until(
@@ -61,13 +118,12 @@ class GoogleMapsCrawler:
 
     def scrape(self, driver):
         has_next_page = True
+        curr_page = 1
         # TODO: make initial checks here
         while has_next_page:
-            self.scrape_results(driver)
-            # TODO: go to next page
-            next_button = driver.find_element(By.XPATH, '//div/button[contains(@aria-label,"Next page")]')
-            next_button.click()
+            self.scrape_results(driver, curr_page)
             sleep(3)
+            curr_page += 1
         # self.scrape_results(driver)
         # # TODO: go to next page
         # next_button = driver.find_element(By.XPATH, '//div/button[contains(@aria-label,"Next page")]')
